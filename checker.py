6e2eece9492319da6a83a776951619d518b90f0d
Drew Maring <andrew.maring@gmail.com>
2019-10-04 11:18:26 -0700

    Factor out token method from each request for better utilization of auth token requests

--- checker.py
+++ checker.py
@@ -18,32 +18,32 @@ def get_auth_token(api_key=Config['api_key'],
     json_ = (result.json())
     return(json_['auth_token'])
 
-def get_user_profile():
-    params = {'auth_token': get_auth_token()}
+def get_user_profile(auth_token=get_auth_token()):
+    params = {'auth_token': auth_token}
     url = 'https://intranet.hbtn.io/users/me.json'
     result = requests.get(url, params=params)
     return(result.json())
 
-def get_project(id):
-    params = {'auth_token': get_auth_token()}
-    url = 'https://intranet.hbtn.io/projects/{}.json'.format(id)
+def get_project (project_id, auth_token=get_auth_token(),):
+    params = {'auth_token': auth_token}
+    url = 'https://intranet.hbtn.io/projects/{}.json'.format(project_id)
     result = requests.get(url, params=params)
     return(result.json())
 
-def get_tasks(id):
-    params = {'auth_token': get_auth_token()}
-    url = 'https://intranet.hbtn.io/tasks/{}.json'.format(id)
+def get_tasks(task_id, auth_token=get_auth_token()):
+    params = {'auth_token': auth_token}
+    url = 'https://intranet.hbtn.io/tasks/{}.json'.format(task_id)
     result = requests.get(url, params=params)
     return(result.json())
 
-def request_correction(id):
-    params = {'auth_token': get_auth_token()}
-    url = 'https://intranet.hbtn.io/tasks/{}/start_correction.json'.format(id)
+def request_correction(task_id, auth_token=get_auth_token()):
+    params = {'auth_token': auth_token}
+    url = 'https://intranet.hbtn.io/tasks/{}/start_correction.json'.format(task_id)
     result = requests.post(url, params=params)
     return(result.json())
 
-def get_correction_result(id):
-    params = {'auth_token': get_auth_token()}
-    url = 'https://intranet.hbtn.io/correction_requests/{}.json'.format(id)
+def get_correction_result(correction_id, auth_token=get_auth_token()):
+    params = {'auth_token': get_auth_token}
+    url = 'https://intranet.hbtn.io/correction_requests/{}.json'.format(correction_id)
     result = requests.get(url, params=params)
     return(result.json())
