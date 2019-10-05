1775819cdc5ff79a4cbd4624d5e8062e44181413
Drew Maring <andrew.maring@gmail.com>
2019-10-04 17:17:12 -0700

    Add giphy api to add random gif to slack message

---
+++ giphy.py
@@ -0,0 +1,14 @@
+import requests
+import random
+
+
+def get_giphy(search):
+    params = {
+        'api_key': '0i82h54aF0m2Gxz68XB4H0x7lc3EI4Nd',
+        'q': search,
+        'limit': 50
+    }
+    url = 'http://api.giphy.com/v1/gifs/search'
+    res = requests.get(url, params=params)
+    num = random.choice(range(50))
+    return(res.json()['data'][num]['embed_url'])
