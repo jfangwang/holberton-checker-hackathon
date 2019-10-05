1f390d970723face567182b5f7814ea459ac87d1
ethanpasta <40837133+ethanpasta@users.noreply.github.com>
2019-10-04 20:48:51 -0700

    Update app.py

--- flask/app.py
+++ flask/app.py
@@ -2,6 +2,7 @@
 from flask import Flask, render_template, request, redirect, session
 from checker import *
 import re
+import json
 import time
 app = Flask(__name__)
 
@@ -9,14 +10,18 @@ app = Flask(__name__)
 def login():
     if request.method == 'POST':
         api_key = request.form['api_key']
+        session['api_key'] = api_key
         email = request.form['email']
+        session['email'] = email
         password = request.form['usr_pwd']
+        session['password'] = password
         user = get_auth_token(api_key, email, password)
         if not user:
             err = "Error: user information is not valid"
             return render_template('login.html', error=err)
         else:
-            session['auth_token'] = user
+            session['auth_token'] = user.get('auth_token')
+            session['user_name'] = user.get('full_name')
             return redirect('/project')
     else:
         return render_template('login.html')
@@ -28,6 +33,7 @@ def project():
     if request.method == 'POST':
             project = request.form['project']
             task_num = request.form['task_num']
+            session['task_position'] = task_num
             rege = r"^(https://)?intranet\.hbtn\.io/projects/(\d+)$"
             match = re.search(rege, project)
             if not match or not task_num.isdigit():
@@ -35,15 +41,30 @@ def project():
             else:
                 project_id = match.group(2)
                 project_j = get_project(project_id, session['auth_token'])
+                session['project_name'] = project_j.get('name')
                 try:
                     task = project_j.get('tasks')[int(task_num)]
                     task_id = task.get('id')
                     try_post = request_correction(task_id, session['auth_token'])
-                    get_results = get_correction_result(task_id, session['auth_token'])
-                    while get_results.get('status') not in ["Done", "Fail"]:
+                    task_corr_id = try_post.get('id')
+                    get_results = get_correction_result(task_corr_id, session['auth_token'])
+                    if get_results.get('delay'):
+                        time.sleep(get_results.get('delay'))
+                    while get_results.get('status') != "Done" and get_results.get('status') != "Fail":
                         time.sleep(1)
-                        get_results = get_correction_result(task_id, session['auth_token'])
-                    return get_results.get('result_display')
+                        get_results = get_correction_result(task_corr_id, session['auth_token'])
+                    checks = get_results.get('result_display').get('checks')
+                    passed = 0
+                    for check in checks:
+                        if check.get('passed'):
+                            passed += 1
+                    big_dict = {}
+                    big_dict['user_name'] = session['user_name']
+                    big_dict['project_name'] = session['project_name']
+                    big_dict['task_position'] = session['task_position']
+                    big_dict['completed_checks'] = passed
+                    big_dict['total_checks'] = len(checks)
+                    """ FINAL DICTIONARY HERE IN big_dict """
                 except Exception:
                     return render_template('project.html', error="Are you sure this task exists?")
     else:
